aba68c045436984a214d2d52281bae1ea288edbd
tulipg2240 <tulipg2240@gmail.com>
2021-12-29 13:43:05 +0900

    ioc 적용전 일반적인 제어

---
+++ src/kr/co/ezenac/ioc/Main.java
@@ -0,0 +1,14 @@
+package kr.co.ezenac.ioc;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "www.google.com/books/it?page=100&name=springframework";
+
+        Encoder encoder = new Encoder();
+        String result = encoder.encode(url);
+
+        System.out.println(result);
+    }
+}
