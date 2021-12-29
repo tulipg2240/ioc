31853bd0cdbe5653a42d56ba151acb9c2e5769fc
tulipg2240 <tulipg2240@gmail.com>
2021-12-29 22:28:33 +0900

    엔코딩 기능을 하는 다른 클래스 생성

--- src/kr/co/ezenac/ioc/Main.java
+++ src/kr/co/ezenac/ioc/Main.java
@@ -1,14 +1,21 @@
 package kr.co.ezenac.ioc;
 
+import java.net.URLEncoder;
+
 public class Main {
 
     public static void main(String[] args) {
 
         String url = "www.google.com/books/it?page=100&name=springframework";
 
+        //Base64 encoding
         Encoder encoder = new Encoder();
         String result = encoder.encode(url);
-
         System.out.println(result);
+
+        //url encoding
+        UrlEncoder urlEncoder = new UrlEncoder();
+        String urlResult = urlEncoder.encode(url);
+        System.out.println(urlResult);
     }
 }
---
+++ src/kr/co/ezenac/ioc/UrlEncoder.java
@@ -0,0 +1,16 @@
+package kr.co.ezenac.ioc;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class UrlEncoder {
+
+    public String encode(String message) {
+        try {
+            return URLEncoder.encode(message, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
